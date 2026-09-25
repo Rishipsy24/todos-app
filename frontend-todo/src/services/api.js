c0068846a4ac42ddac5a9641aef3e94560186cc0
Rishipsy24 <yoyome2024@gmail.com>
2026-09-26 01:10:19 +0530

    Fix production API URL

--- frontend-todo/src/services/api.js
+++ frontend-todo/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://todos-app-uuwf.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -8,9 +8,11 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
